metaeducation/ren-c

Clarify Temp_UTF8_At_Managed interface
Historically, Rebol strings were either one-byte-per-codepoint Latin1,
or two-bytes-per-codepoint UCS2.  A routine would create a temporary
BINARY! series holding UTF-8 data when needed, that captured a part of
that string and converted it if a conversion was necessary.  If it was
not necessary, it would return the offset into the existing byte-sized
series.

The interface of this function was somewhat awkward, since it could
create a new series (where the offset of the data start would be 0) and
it "transformed" an incoming length in codepoints to an outgoing
"length" in bytes.  With UTF-8 everywhere, there will be no need to
make a new series, just to determine where an index into a string
translates to as an offset in bytes, and how a length at that index
translates to a size in bytes.

So this simplifies the interface as a more obvious extraction of the
two input properties (string value and length in codepoints limit) and
the two output properties (offset in bytes and size in bytes of the
corresponding range).

It also rewrites Temp_Byte_Chars_May_Fail() to become a more UTF-8
Everywhere friendly Analyze_String_For_Scan() interface.  This routine
is used to prepare STRING! values to be re-scanned by the scanner in
various MAKE or TO functions, such as MAKE of a DATE! from a STRING!...
which will involve less conversion with UTF8-Everywhere.

src/core/t-decimal.c
@@ -170,19 +170,15 @@ void MAKE_Decimal(REBVAL *out, enum Reb_Kind kind, const REBVAL *arg) {
         d = VAL_NANO(arg) * NANO;
         break;
 
-    case REB_STRING:
-        {
-        REBCNT len;
-        REBYTE *bp = Temp_Byte_Chars_May_Fail(
-            arg, MAX_SCAN_DECIMAL, &len, FALSE
-        );
+    case REB_STRING: {
+        REBSIZ size;
+        REBYTE *bp = Analyze_String_For_Scan(&size, arg, MAX_SCAN_DECIMAL);
 
-        if (NULL == Scan_Decimal(out, bp, len, LOGICAL(kind != REB_PERCENT)))
+        if (NULL == Scan_Decimal(out, bp, size, LOGICAL(kind != REB_PERCENT)))
             goto bad_make;
 
         d = VAL_DECIMAL(out); // may need to divide if percent, fall through
-        break;
-        }
+        break; }
 
     case REB_BINARY:
         if (VAL_LEN_AT(arg) < 8)

rhencke/rebol

src/core/t-decimal.c
@@ -172,19 +172,15 @@ void MAKE_Decimal(REBVAL *out, enum Reb_Kind kind, const REBVAL *arg) {
         d = VAL_NANO(arg) * NANO;
         break;
 
-    case REB_STRING:
-        {
-        REBCNT len;
-        REBYTE *bp = Temp_Byte_Chars_May_Fail(
-            arg, MAX_SCAN_DECIMAL, &len, FALSE
-        );
+    case REB_STRING: {
+        REBSIZ size;
+        REBYTE *bp = Analyze_String_For_Scan(&size, arg, MAX_SCAN_DECIMAL);
 
-        if (NULL == Scan_Decimal(out, bp, len, DID(kind != REB_PERCENT)))
+        if (NULL == Scan_Decimal(out, bp, size, DID(kind != REB_PERCENT)))
             goto bad_make;
 
         d = VAL_DECIMAL(out); // may need to divide if percent, fall through
-        break;
-        }
+        break; }
 
     case REB_BINARY:
         if (VAL_LEN_AT(arg) < 8)