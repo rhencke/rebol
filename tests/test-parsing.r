156b8c8297f9eb9698b83df9b922d86affacaef8
Brian Dickens <hostilefork@gmail.com>
2018-05-21 21:13:21 -0400

    Tolerate tags in test groups, delete old test conversion code
    In the initial writing, ISSUE! was allowed in test groups (used for
    linking tests to their GitHub/CureCode issue IDs).  Tags were not
    allowed in these groups, however... only between them.
    
    Also deletes the code for converting from the old non-GROUP!-able test
    block format to the new format.  There are no other tests outstanding
    to convert--and it makes the source more confusing to have two slightly
    different copies of the same code.

--- tests/test-parsing.r
+++ tests/test-parsing.r
@@ -33,7 +33,9 @@ make object! [
 
     set 'test-source-rule [
         any [
-            position: ["{" | {"}] (
+            position:
+
+            ["{" | {"}] (
                 ; handle string using TRANSCODE
                 success: either error? trap [
                     position: second transcode/next position
@@ -46,331 +48,22 @@ make object! [
                 |
             ["{" | {"}] :position break
                 |
-            "[" test-source-rule "]"
+            "[" test-source-rule "]" ;-- plain BLOCK! in code for a test
                 |
-            "(" test-source-rule ")"
+            "(" test-source-rule ")" ;-- plain GROUP! in code for a test
                 |
             ";" [thru newline | to end]
                 |
+            ;
+            ; If we see a closing bracket out of turn, that means we've "gone
+            ; too far".  It's either a syntax error, or the closing bracket of
+            ; a multi-test block.
+            ;
             "]" :position break
                 |
             ")" :position break
                 |
             skip
-        ]
-    ]
-
-    ; -----------------------------------------------------------------------
-    ; Temporary code section
-    ; The following scanner, tokeniser and renderer are used for conversion
-    ; to the new test format (grouped tests).
-    ; It's presence allows developers to upgrade their own test code bases,
-    ; once that has been done this code can be removed.
-
-    make-original-tests-scanner: function [
-        {Create a scanner for original test format.}
-    ][
-        context [
-            type: ; Token type.
-            t1: t2: _ ; Token begin and end.
-            d1: d2: _ ; Token data begin and end.
-            value: _ ; Token value.
-            eol: _
-
-            position: next-position: _
-
-            do-scanned: _ ; Token scan event function.
-            types: context [eol: wsp: cmt: tst: flg: fil: end: _]
-
-            single-value: parsing-at x [
-                if not error? trap [
-                    set [value: next-position:] transcode/next x
-                ][
-                    next-position
-                ]
-            ]
-
-            rule: [
-                ; EOL convention.
-                position:
-                any not-eolc
-                [CR LF (
-                    eol: unspaced [CR LF]
-                )
-                | LF (
-                    eol: LF
-                )]
-                :position
-
-                any [
-                    (type: value: t2: d2: _) t1: d1:
-                    [
-                        eol (type: in types 'eol)
-                            |
-                        some whitespace (type: in types 'wsp)
-                            |
-                        ";" d1: [to eol d2: thru newline | to end d2:] (type: in types 'cmt)
-                            |
-                        ["[" d1: test-source-rule d2: "]" ] (type: in types 'tst)
-                            |
-                        end (type: in types 'end) break
-                            |
-                        position: single-value d2: [
-                            if (issue? get 'value) (type: in types 'flg)
-                            |
-                            if (file? get 'value) (type: in types 'fil)
-                        ]
-                    ]
-                    t2: (
-                        if blank? type [
-                            fail [
-                                {Scanning failed at line/col} text-location-of t1
-                                {near} mold copy/part to text! t1 80
-                            ]
-                        ]
-                        do-scanned
-                    )
-                    :t2 ; do-scanned may have changed position of t2.
-                ]
-            ]
-
-            not-eolc: complement charset reduce [CR LF]
-
-            scan: procedure [
-                {Scan the contents for basic tokens.}
-                contents [binary!]
-            ] [
-                assert [parse contents rule]
-            ]
-        ]
-    ]
-
-    make-original-tests-tokeniser: function [
-        {Modifies basic syntax tokens from scanner into final tokens.}
-    ][
-        context [
-
-            num: ids: _
-            note: _
-            test: code: pattern: txt: _
-            issue-id: ["bug#" copy num some digit]
-            position: _
-
-            scanner: make-original-tests-scanner
-
-            ttype: tvalue: _
-            do-token: _
-
-            ; Customise scanner, to reinterpret tokens.
-            do in scanner [
-
-                types/tst: [
-                    ; We want to extract issues encoded as ISSUE! from inside the test.
-                    test: copy/part d1 d2
-                    if attempt [
-                        all [
-                            issue? num: first code: load test
-                            parse next form first code [some digit]
-                            num = do/next code 'position
-                            position = next code
-                        ]
-                    ][
-                        ; Rewrite test to remove issue.
-                        pattern: compose [(mold num) opt [any whitespace #"|"] any whitespace]
-                        parse test [any whitespace remove pattern]
-                        ttype: 'isu
-                        tvalue: compose/deep [[(num)] ""]
-                        do-token
-                    ]
-
-                    ttype: type
-                    tvalue: test
-                    do-token
-                ]
-
-                types/flg: [
-                    ttype: type
-                    tvalue: next mold value
-                    do-token
-                ]
-
-                types/cmt: [
-
-                    ; Extract issues encoded within comments.
-                    txt: trim/head/tail to text! copy/part d1 d2
-                    if find txt "#" [
-                        ids: copy []
-                        if parse txt [
-                            opt #"#" issue-id (append ids num)
-                            any [
-                                {, } issue-id (append ids num)
-                            ]
-                            opt [{ - } | [":" | some "."] some whitespace]
-                            copy note to end
-                            |
-                            copy note some [not [[{, } | {: }] issue-id] skip]
-                            opt {, } issue-id opt #"." (append ids num)
-                        ] [
-                            ttype: 'isu
-                            tvalue: compose/only [(ids) (note)]
-                            do-token
-                        ] else [
-                            ttype: type
-                            tvalue: copy/part d1 d2
-                            do-token
-                        ]
-                    ] else [
-                        ttype: type
-                        tvalue: copy/part d1 d2
-                        do-token
-                    ]
-                ]
-
-                types/eol: [
-                    ttype: type
-                    tvalue: eol
-                    do-token
-                ]
-
-                types/fil: types/wsp: [
-                    ttype: type
-                    tvalue: copy/part t1 t2
-                    do-token
-                ]
-
-                types/end: _
-
-                do-scanned: does [do get type]
-            ]
-        ]
-    ]
-
-    tokenise-original-tests: function [
-        {Returns block of tokens.}
-        test-source [object!]
-    ][
-        do in make-original-tests-tokeniser [
-            collect [
-                do-token: does [keep ttype keep/only tvalue]
-                scanner/scan test-source/contents
-            ]
-        ]
-    ]
-
-    test-renderer: context [
-        output: copy #{}
-        issues: copy []
-        eol: newline
-        last-emit: _
-
-        emit: func [value][
-            append output last-emit: value
-        ]
-
-        render: function [tokens][
-            clear output
-            parse tokens [thru 'eol set eol [text! | char!]]
-            for-each [type val] tokens [
-                switch/default type [
-                    'eol 'wsp [emit val]
-                    'cmt [emit #";" emit val emit eol]
-                    'tst [
-                        replace/all val newline eol
-                        if empty? issues [
-                            if eol <> last-emit [emit eol]
-                            emit #"(" emit val emit #")"
-                        ] else [
-                            if eol <> last-emit [emit eol]
-                            emit "["
-                            state: #issues-only
-                            for-each issue issues [
-                                if state = #issues-only [
-                                    if not empty? issue/2 [
-                                        state: #issues-and-note
-                                    ]
-                                ] else [
-                                    if not empty? issue/2 [
-                                        state: #issue-lines
-                                    ]
-                                ]
-                            ]
-                            num: 0
-                            for-each issue issues [
-                                if find [#issues-only #issues-and-note] state [
-                                    ; Append issue.
-                                    if 1 < (num: me + 1) [
-                                        emit space
-                                    ]
-                                ] else [
-                                    emit eol emit {    }
-                                ]
-                                emit spaced map-each id issue/1 [to issue! id]
-                                if not empty? note: issue/2 [
-                                    emit spaced [{ ;} note]
-                                ]
-                            ]
-                            clear issues
-
-                            if parse val [to newline to end] [
-                                if state = #issues-only [
-                                    state: #multiline-test
-                                    emit " (" emit val
-                                ] else [
-                                    state: #multiline-indented
-                                    emit eol
-                                    emit "    ("
-                                    emit val
-                                    emit "    "
-                                ]
-                            ] else [
-                                state: #oneline-test
-                                emit eol
-                                emit "    ("
-                                emit val
-                            ]
-                            emit #")"
-                            if state <> #multiline-test [
-                                emit eol
-                            ]
-                            emit #"]"
-                        ]
-                    ]
-                    'fil [emit val]
-                    'flg [emit #"<" emit val emit #">"]
-                    'isu [append/only issues val]
-                    'end []
-                ] [
-                    fail [{Cannot render type } (type)]
-                ]
-            ]
-
-            lock copy output
-        ]
-    ]
-
-    files-upgraded: copy []
-
-    upgrade-test-source: function [
-        {Converts from original format to new convention.}
-        test-source [object!]
-    ][
-        tokens: tokenise-original-tests test-source
-        for i 2 length of tokens 2 [
-            if not block? tk: pick tokens i [
-                poke tokens i to text! pick tokens i
-            ]
-        ]
-
-        test-source/contents: test-renderer/render tokens
-
-        comment [
-            ;; replace test-source/filepath {/C/Projects/Public/ren-c/tests} {/C/Projects/Public/temp.201803-test-format/tests}
-            folder: first split-path test-source/filepath
-            if not exists? folder [
-                make-dir/deep folder
-            ]
-            write test-source/filepath test-source/contents
-            append files-upgraded test-source/filepath
         ]
     ]
 
@@ -382,12 +75,8 @@ make object! [
             filepath: test-file
             contents: read test-file
         ]
-        ;; upgrade-test-source test-source ; TODO: Remove when not needed.
         test-source
     ]
-
-    ; End temporary code section.
-    ; -----------------------------------------------------------------------
 
     set 'collect-tests procedure [
         collected-tests [block!]
@@ -452,7 +141,14 @@ make object! [
             "[" (type: in types 'grpb) emit-token
             any [
                 any-wsp single-value
-                if (issue? value) (type: in types 'isu)
+                [
+                    if (tag? value) (
+                        type: in types 'flag
+                        append flags value
+                    )
+                        |
+                    if (issue? value) (type: in types 'isu)
+                ]
                 emit-token
             ]
             opt [
@@ -465,7 +161,10 @@ make object! [
         ]
 
         token: [
-            position: (type: value: _)
+            position:
+
+            (type: value: _)
+
             wsp emit-token
                 |
             single-test (flags: copy []) emit-token
