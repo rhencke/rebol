7663e29093753edc686fde16d912c160769a1aab
Brian Dickens <hostilefork@gmail.com>
2018-07-11 03:53:07 -0400

    Type check that argument to DOES is accepted by DO
    Writing code like `does http://example.com` was conceived to do the
    same thing as `does [do http://example.com]`, just for consistency.
    So it would generate an on-the-fly specialization of DO.  However, it
    would not type check the argument--which is required of the process
    of specialization itself (otherwise each call to a specialization
    would have to repeat type checking of the specialized-out arguments)

--- src/core/c-specialize.c
+++ src/core/c-specialize.c
@@ -1345,7 +1345,18 @@ REBNATIVE(does)
     }
     else {
         // On all other types, we just make it act like a specialized call to
-        // DO for that value.
+        // DO for that value.  But since we're manually specializing it, we
+        // are responsible for type-checking...the evaluator expects any
+        // specialization process to do so (otherwise it would have to pay
+        // for type checking on each call).
+        //
+        // !!! The error reports that DOES doesn't accept the type for its
+        // specializee argument, vs. that DO doesn't accept it.
+        //
+        REBVAL *typeset = ACT_PARAM(NAT_ACTION(do), 1);
+        REBVAL *param = PAR(specializee);
+        if (not TYPE_CHECK(typeset, VAL_TYPE(specializee)))
+            fail (Error_Arg_Type(frame_, param, VAL_TYPE(specializee)));
 
         exemplar = Make_Context_For_Action(
             NAT_VALUE(do),
