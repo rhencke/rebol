13e22ccc5699348c7e86ecaf3ec7d53b863101d2
Giulio Lunati <giuliolunati@gmail.com>
2018-08-23 13:53:05 +0200

    Emscripten build: build libr3.* instead of r3.*

---
+++ make/configs/emscripten-debug.r
@@ -0,0 +1,35 @@
+REBOL []
+
+os-id: 0.16.2
+
+gcc-path: 
+
+toolset: [
+    gcc %emcc
+    ld %emcc
+]
+
+optimize: "z"
+
+ldflags: reduce [
+    unspaced ["-O" optimize]
+    {-s 'ASSERTIONS=1'}
+    {-s 'EXTRA_EXPORTED_RUNTIME_METHODS=["ccall", "cwrap"]'}
+    {--post-js prep/include/reb-lib.js}
+]
+
+extensions: [
+    - crypt _
+    - process _
+    - view _
+    - png _
+    - gif _
+    - jpg _
+    - bmp _
+    - locale _
+    - uuid _
+    - odbc _
+    - ffi _
+    - debugger _
+    - Clipboard _
+]
